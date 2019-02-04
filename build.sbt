171feda59d0432144d3fa5a6d6278065c903ba28
Karen Painter <klpainter@gmail.com>
2019-02-03 19:34:17 -0800

    Multiple endpoints

--- build.sbt
+++ build.sbt
@@ -1,12 +1,14 @@
-libraryDependencies += "com.nike.riposte" % "riposte-core" % "0.15.0"
-libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.0"
-libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
-
 name := "ScalaEndpoint"
 
 version := "0.1"
 
 scalaVersion := "2.12.8"
+
+libraryDependencies ++= Seq (
+  "com.nike.riposte" % "riposte-core" % "0.15.0",
+  "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.0",
+  "org.scalatest" %% "scalatest" % "3.0.5" % "test"
+)
 
 
 
--- src/main/scala/server/Main.scala
+++ src/main/scala/server/Main.scala
@@ -1,5 +1,6 @@
 package server
 
+import java.util
 import java.util.concurrent.{CompletableFuture, Executor}
 
 import com.nike.riposte.server.Server
@@ -8,13 +9,17 @@ import com.nike.riposte.server.http.{Endpoint, RequestInfo, ResponseInfo, Standa
 import com.nike.riposte.util.Matcher
 import io.netty.channel.ChannelHandlerContext
 
+import scala.collection.immutable.HashSet
+
 object Main extends App {
   val server = new Server(AppServerConfig)
   server.startup()
 }
 
 object AppServerConfig extends ServerConfig {
-  val endpoints: java.util.Collection[Endpoint[_]] = java.util.Collections.singleton(HelloWorldEndpoint)
+  val endpoints: java.util.Set[Endpoint[_]] = new util.HashSet[Endpoint[_]]()
+  endpoints.add(HelloWorldEndpoint)
+  endpoints.add(SecondEndpoint)
 
   override def appEndpoints(): java.util.Collection[Endpoint[_]] = endpoints
 }
@@ -34,3 +39,21 @@ object HelloWorldEndpoint extends StandardEndpoint[Void, String] {
     )
   }
 }
+
+object SecondEndpoint extends StandardEndpoint[Void, String] {
+  override def requestMatcher(): Matcher = Matcher.`match`("/secondEndpoint")
+
+  override def execute(
+                        request: RequestInfo[Void],
+                        longRunningTaskExecutor: Executor,
+                        ctx: ChannelHandlerContext): CompletableFuture[ResponseInfo[String]] =
+  {
+    CompletableFuture.completedFuture(
+      ResponseInfo.newBuilder("Hello again, Karen!")
+        .withDesiredContentWriterMimeType("text/plain")
+        .build()
+    )
+  }
+}
+
+
