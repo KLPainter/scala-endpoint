3ef6440bdc3e1bbf3e47b8fbd2afcf5c5b57ddc0
Karen Painter <klpainter@gmail.com>
2019-02-05 12:14:05 -0800

    successfully calling Ron Swanson api

--- build.sbt
+++ build.sbt
@@ -6,6 +6,7 @@ scalaVersion := "2.12.8"
 
 libraryDependencies ++= Seq (
   "com.nike.riposte" % "riposte-core" % "0.15.0",
+  "com.nike.riposte" % "riposte-async-http-client" % "0.15.0",
   "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.0",
   "org.scalatest" %% "scalatest" % "3.0.5" % "test"
 )
--- src/main/scala/server/Main.scala
+++ src/main/scala/server/Main.scala
@@ -6,10 +6,13 @@ import java.util.concurrent.{CompletableFuture, Executor}
 import com.nike.riposte.server.Server
 import com.nike.riposte.server.config.ServerConfig
 import com.nike.riposte.server.http.{Endpoint, RequestInfo, ResponseInfo, StandardEndpoint}
+import com.nike.riposte.client.asynchttp.ning.AsyncHttpClientHelper
+import com.nike.riposte.client.asynchttp.ning.RequestBuilderWrapper
+import com.nike.riposte.server.http.impl.FullResponseInfo
 import com.nike.riposte.util.Matcher
+import com.ning.http.client.Response
 import io.netty.channel.ChannelHandlerContext
-
-import scala.collection.immutable.HashSet
+import io.netty.handler.codec.http.HttpMethod
 
 object Main extends App {
   val server = new Server(AppServerConfig)
@@ -19,7 +22,7 @@ object Main extends App {
 object AppServerConfig extends ServerConfig {
   val endpoints: java.util.Set[Endpoint[_]] = new util.HashSet[Endpoint[_]]()
   endpoints.add(HelloWorldEndpoint)
-  endpoints.add(SecondEndpoint)
+  endpoints.add(ApiEndpoint)
 
   override def appEndpoints(): java.util.Collection[Endpoint[_]] = endpoints
 }
@@ -40,20 +43,37 @@ object HelloWorldEndpoint extends StandardEndpoint[Void, String] {
   }
 }
 
-object SecondEndpoint extends StandardEndpoint[Void, String] {
-  override def requestMatcher(): Matcher = Matcher.`match`("/secondEndpoint")
+object ApiEndpoint extends StandardEndpoint[Void, String] {
+  override def requestMatcher(): Matcher = Matcher.`match`("/apiEndpoint")
+  val asyncHttpClientHelper = new AsyncHttpClientHelper
 
   override def execute(
                         request: RequestInfo[Void],
                         longRunningTaskExecutor: Executor,
-                        ctx: ChannelHandlerContext): CompletableFuture[ResponseInfo[String]] =
-  {
-    CompletableFuture.completedFuture(
-      ResponseInfo.newBuilder("Hello again, Karen!")
-        .withDesiredContentWriterMimeType("text/plain")
-        .build()
-    )
+                        ctx: ChannelHandlerContext): CompletableFuture[ResponseInfo[String]] = {
+
+    def buildRequest(downstreamUrl: String, method: HttpMethod): RequestBuilderWrapper = {
+      asyncHttpClientHelper.getRequestBuilder(downstreamUrl, method)
+    }
+    val reqWrapper = buildRequest("https://ron-swanson-quotes.herokuapp.com/v2/quotes", HttpMethod.GET)
+
+    asyncHttpClientHelper.executeAsyncHttpRequest(reqWrapper, (asyncResponse: Response) => {
+      def handleResponse(asyncResponse: Response) :FullResponseInfo[String] = {
+        ResponseInfo.newBuilder(asyncResponse.getResponseBody).build
+      }
+      handleResponse(asyncResponse)
+    }, ctx)
   }
+
+
+  // timer stuff
+  //  logger.info("About to make async library call")
+  //  val startTime = new ExampleDownstreamHttpAsyncEndpoint.ObjectHolder[Long]
+  //  startTime.heldObject = System.currentTimeMillis
+  //logger.info("In async response handler. Total time spent millis: {}", System.currentTimeMillis - startTime.heldObject)
+
+  // additional buildRequest stuff
+  //    reqWrapper.requestBuilder.addQueryParam("some_query_param", "foo").addHeader("Accept", "application/json")
+  //    if (request.getRawContentLengthInBytes > 0) reqWrapper.requestBuilder.setBody(request.getRawContent)
+
 }
-
-
